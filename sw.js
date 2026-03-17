aa4cdc3724e68b0390de08ec376cdb2cfd2b5e04
srikarkovirineni-cloud <srikarkovirineni@gmail.com>
2026-03-16 19:09:36 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,17 @@
-const CACHE = 'getfit-v3';
+const CACHE = 'getfit-v4';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll([
-      '/getfit/index.html',
-      '/getfit/manifest.json'
-    ]).catch(() => {}))
-  );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() =>
-      caches.match(e.request).then(cached =>
-        cached || caches.match('/getfit/index.html')
-      )
-    )
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match('/')));
 });
