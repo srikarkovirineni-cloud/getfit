7675796f1f2f6273a243ea11db53ba4e9cfe310e
srikarkovirineni-cloud <srikarkovirineni@gmail.com>
2026-03-17 15:16:23 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
 const CACHE = 'getfit-v4';
 
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
+self.addEventListener('install', e => { self.skipWaiting(); });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
@@ -13,5 +11,12 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match('/')));
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request).then(c => c || caches.match('/')))
+  );
 });
